1ea31973a62cc84a8dd38cd81dd24051a7e81e9d
Shameena Koodan <shameena.koodan@gmail.com>
2023-09-27 12:36:22 -0500

    Added the service files and its implementation for user registration

--- src/main/java/org/shameenakoodan/KanbanFlowUserService/impl/UserServiceImpl.java
+++ src/main/java/org/shameenakoodan/KanbanFlowUserService/impl/UserServiceImpl.java
@@ -1,9 +1,76 @@
 package org.shameenakoodan.KanbanFlowUserService.impl;
+import org.shameenakoodan.KanbanFlowUserService.dto.UserDTO;
+import org.shameenakoodan.KanbanFlowUserService.model.Role;
+import org.shameenakoodan.KanbanFlowUserService.model.User;
+import org.shameenakoodan.KanbanFlowUserService.repository.RoleRepository;
+import org.shameenakoodan.KanbanFlowUserService.repository.UserRepository;
+import org.shameenakoodan.KanbanFlowUserService.service.UserService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Service;
+
 @Service
-public class StudentServiceImpl {
+public class UserServiceImpl implements UserService {
+    private UserRepository userRepository;
+    private RoleRepository roleRepository;
+    private PasswordEncoder passwordEncoder;
+
+    public UserServiceImpl(UserRepository userRepository, RoleRepository roleRepository,
+                              PasswordEncoder passwordEncoder) {
+        super();
+        this.userRepository = userRepository;
+        this.roleRepository = roleRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    public void saveStudent(UserDTO userDTO) {
+        User user = new User();
+
+        user.setName(userDTO.getFirstName() + " " + userDTO.getLastName());
+        user.setEmail(userDTO.getEmail());
+
+        // Encrypt the password using Spring Security
+        user.setPassword(passwordEncoder.encode(userDTO.getPassword()));
+        Role role = roleRepository.findByName("ROLE_ADMIN");
+        if (role == null) {
+            role = checkRoleExist();
+        }
+        user.setRoles(Arrays.asList(role));
+        userRepository.save(user);
+    }
+
+    private Role checkRoleExist() {
+        Role role = new Role();
+        role.setName("ROLE_ADMIN");
+        return roleRepository.save(role);
+    }
+
+    @Override
+    public User findStudentByEmail(String email) {
+
+        return userRepository.findByEmail(email);
+    }
+
+    @Override
+    public List<UserDTO> findAllStudents() {
+        List<User> students = userRepository.findAll();
+
+        return students.stream()
+                .map((user) -> mapToStudentDto(user))
+                .collect(Collectors.toList());
+    }
+
+    private UserDTO mapToStudentDto(User user) {
+        UserDTO userDTO = new UserDTO();
+
+        String[] str = user.getName().split(" ");
+        userDTO.setFirstName(str[0]);
+        userDTO.setLastName(str[1]);
+        userDTO.setEmail(user.getEmail());
+        return userDTO;
+    }
 }
--- src/main/java/org/shameenakoodan/KanbanFlowUserService/service/UserService.java
+++ src/main/java/org/shameenakoodan/KanbanFlowUserService/service/UserService.java
@@ -1,2 +1,11 @@
-package org.shameenakoodan.KanbanFlowUserService.service;public class UserService {
+package org.shameenakoodan.KanbanFlowUserService.service;
+import org.shameenakoodan.KanbanFlowUserService.dto.UserDTO;
+import org.shameenakoodan.KanbanFlowUserService.model.User;
+
+import java.util.List;
+
+public interface UserService {
+    void saveStudent(UserDTO studentDto);
+    User findStudentByEmail(String email);
+    List<UserDTO> findAllStudents();
 }
