35a3e4cea2e9daddc4d55036c3642909fc791ab3
Shameena Koodan <shameena.koodan@gmail.com>
2023-09-27 12:37:00 -0500

    Added the security files for user registration

--- src/main/java/org/shameenakoodan/KanbanFlowUserService/security/CustomUserDetailsService.java
+++ src/main/java/org/shameenakoodan/KanbanFlowUserService/security/CustomUserDetailsService.java
@@ -1,2 +1,34 @@
-package org.shameenakoodan.KanbanFlowUserService.security;public class CustomUserDetailsService {
+package org.shameenakoodan.KanbanFlowUserService.security;
+
+import org.shameenakoodan.KanbanFlowUserService.model.User;
+import org.shameenakoodan.KanbanFlowUserService.repository.UserRepository;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.stream.Collectors;
+
+public class CustomUserDetailsService implements UserDetailsService {
+    private UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        super();
+        this.userRepository = userRepository;
+    }
+    @Override
+    public UserDetails loadUserByUsername(String usernameOrEmail) throws UsernameNotFoundException {
+
+        User student = userRepository.findByEmail(usernameOrEmail);
+        if(student != null) {
+            return new org.springframework.security.core.userdetails.User(student.getEmail(),
+                    student.getPassword(),
+                    student.getRoles().stream()
+                            .map((role) -> new SimpleGrantedAuthority(role.getName()))
+                            .collect(Collectors.toList()));
+        }else {
+            throw new UsernameNotFoundException("Invalid email or password");
+        }
+    }
+
 }
